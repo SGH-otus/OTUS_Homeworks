61f2d9116daced2167d1c81c9b548cb73f0122fd
SGH-otus <57526286+SGH-otus@users.noreply.github.com>
2020-02-19 16:36:52 +0300

    Add files via upload

--- 6_ShellSort/ShellSort.py
+++ 6_ShellSort/ShellSort.py
@@ -1,25 +1,25 @@
 import random
 import time
 import math
+import sys
 
-size = 40000 # size of array to sort
-minrand = -100000 # minimum integer to generate as array member
-maxrand = 100000 # maximum integer to generate as array member
-
-def gen_rand_arr():
+def gen_rand_arr(size, minrand, maxrand):
     arr = []
-    for i in range(size):
+    for i in xrange(size):
         arr.append(random.randint(minrand,maxrand))
     return arr
     
-def mixelems(cnt): # mix 'cnt' random elements in the array
-    global arr
-    for i in range(cnt):
-        pos = random.randint(0, size - 1)
-        t = arr[pos]
-        del arr[pos]
-        pos = random.randint(0, size - 1)
-        arr.insert(pos, t)    
+def mixelems(arr, cnt): # mix 'cnt' random elements in the array
+    sys.stdout.write("Mixing... ")
+    for i in xrange(cnt):
+        pos1 = random.randint(0, len(arr) - 1)
+        pos2 = random.randint(0, len(arr) - 1)
+        t = arr[pos1]
+        arr[pos2] = arr[pos1]
+        arr[pos1] = t
+    sys.stdout.write("\r             ")
+    sys.stdout.write("\r")    
+    sys.stdout.flush()
 
 def gen_smooth(max): # genereate 3-smooth numbers not greather then 'max', for Pratt's gaps
     n2 = []
@@ -38,16 +38,15 @@ def gen_smooth(max): # genereate 3-smooth numbers not greather then 'max', for P
 
     for i2 in n2:
         for i3 in n3:
-            if i2 * i3<size:
+            if i2 * i3 < max:
                 n.append(i2 * i3)
     return sorted(n + n2 + n3)[::-1]
 
-def ShellSort_Shell(): # classic Shell, gaps = N / 2**k
-    global arr # sort orignal array, instead of copy, how it is passed to function
-    gap = size / 2
+def ShellSort_Shell(arr): # classic Shell, gaps = N / 2**k
+    gap = len(arr) / 2
     while gap > 0:
         i = 0
-        while (i + gap < size):
+        while (i + gap < len(arr)):
             j = i + gap
             tmp = arr[j]
             while ((j - gap >= 0) and (arr[j - gap] > tmp)):
@@ -56,13 +55,12 @@ def ShellSort_Shell(): # classic Shell, gaps = N / 2**k
             i = i + 1
         gap = gap / 2
 
-def ShellSort_Hibbard(): # Shell with Hibbard gaps = (2**k) - 1 (OEIS A168604)
-    global arr
-    gap = int(math.log(size,2)) 
+def ShellSort_Hibbard(arr): # Shell with Hibbard gaps = (2**k) - 1 (OEIS A168604)
+    gap = int(math.log(len(arr), 2)) 
     gap = 2**gap - 1
     while gap > 0:
         i = 0
-        while (i + gap < size):
+        while (i + gap < len(arr)):
             j = i + gap
             tmp = arr[j]
             while ((j - gap >= 0) and (arr[j - gap] > tmp)):
@@ -71,12 +69,11 @@ def ShellSort_Hibbard(): # Shell with Hibbard gaps = (2**k) - 1 (OEIS A168604)
             i = i + 1
         gap = gap / 2
 
-def ShellSort_Pratt_smooth(): # Shell with Pratt gaps 3-smooth numbers (OEIS A003586)
-    global arr
-    smooth  = gen_smooth(size)
+def ShellSort_Pratt_smooth(arr): # Shell with Pratt gaps 3-smooth numbers (OEIS A003586)
+    smooth = gen_smooth(len(arr))
     for gap in smooth:
         i = 0
-        while (i + gap < size):
+        while (i + gap < len(arr)):
             j = i + gap
             tmp = arr[j]
             while ((j - gap >= 0) and (arr[j - gap] > tmp)):
@@ -84,70 +81,76 @@ def ShellSort_Pratt_smooth(): # Shell with Pratt gaps 3-smooth numbers (OEIS A00
                 j = j - gap
             i = i + 1
 
-print "%d elements [ %d ; %d ]" % (size, minrand, maxrand)
+# MAIN
+
+size = 40000 # size of array to sort
+minrand = -100000 # minimum integer to generate as array member
+maxrand = 100000 # maximum integer to generate as array member
+mix_coeff1 = 10 # how many elements to mix
+mix_coeff2 = 5 # how many elements to mix
+
+arr = gen_rand_arr(size, minrand, maxrand)
+print "Generated %d elements [ %d ; %d ]" % (size, minrand, maxrand)
+print
+print "Name \t\t\t\t\t Time \t\t Correct"
+print "=" * 64
 
 # PURE SHELL
-arr = gen_rand_arr()
-
-print
 start_time = time.time()
-ShellSort_Shell()
+ShellSort_Shell(arr)
 end_time = time.time() - start_time
-print "Sorted correctly: ", arr == sorted(arr)
-print "ShellSort (Shell) of random: %0.5f" % (end_time)
+print "ShellSort (Shell) of random: \t\t %0.5f \t %r" % (end_time, arr == sorted(arr))
 
-mixelems(size / 10)
+mixelems(arr, size / mix_coeff1)
 start_time = time.time()
-ShellSort_Shell()
+ShellSort_Shell(arr)
 end_time = time.time() - start_time
-print "ShellSort (Shell) of 10 %% random: %0.5f" % (end_time)
+print "ShellSort (Shell) of %0.2f %% random: \t %0.5f \t %r" % ((100.0) / mix_coeff1, end_time, arr == sorted(arr))
 
-mixelems(5)
+mixelems(arr, 5)
 start_time = time.time()
-ShellSort_Shell()
+ShellSort_Shell(arr)
 end_time = time.time() - start_time
-print "ShellSort (Shell) of 5 random: %0.5f" % (end_time)
+print "ShellSort (Shell) of %d random: \t\t %0.5f \t %r" % (mix_coeff2, end_time, arr == sorted(arr))
     
 # HIBBARD
 print
-arr = gen_rand_arr()
+arr = gen_rand_arr(size, minrand, maxrand)
 
 start_time = time.time()
-ShellSort_Hibbard()
+ShellSort_Hibbard(arr)
 end_time = time.time() - start_time
-print "Sorted correctly: ", arr == sorted(arr)
-print "ShellSort (Hibbard) of random: %0.5f" % (end_time), arr == sorted(arr)
+print "ShellSort (Hibbard) of random: \t\t %0.5f \t %r" % (end_time, arr == sorted(arr))
 
-mixelems(size / 10)
+mixelems(arr, size / mix_coeff1)
 start_time = time.time()
-ShellSort_Hibbard()
+ShellSort_Hibbard(arr)
 end_time = time.time() - start_time
-print "ShellSort (Hibbard) of 10 %% random: %0.5f" % (end_time)
+print "ShellSort (Shell) of %0.2f %% random: \t %0.5f \t %r" % ((100.0) / mix_coeff1, end_time, arr == sorted(arr))
 
-mixelems(5)
+mixelems(arr, 5)
 start_time = time.time()
-ShellSort_Hibbard()
+ShellSort_Hibbard(arr)
 end_time = time.time() - start_time
-print "ShellSort (Hibbard) of 5 random: %0.5f" % (end_time)
+print "ShellSort (Shell) of %d random: \t\t %0.5f \t %r" % (mix_coeff2, end_time, arr == sorted(arr))
     
 # PRATT SMOOTH
 print
-arr = gen_rand_arr()
+arr = gen_rand_arr(size, minrand, maxrand)
 
 start_time = time.time()
-ShellSort_Pratt_smooth()
+ShellSort_Pratt_smooth(arr)
 end_time = time.time() - start_time
-print "Sorted correctly: ", arr == sorted(arr)
-print "ShellSort (Pratt Smooth) of random: %0.5f" % (end_time), arr == sorted(arr)
+print "ShellSort (Pratt Smooth) of random: \t %0.5f \t %r" % (end_time, arr == sorted(arr))
 
-mixelems(size / 10)
+mixelems(arr, size / mix_coeff1)
 start_time = time.time()
-ShellSort_Pratt_smooth()
+ShellSort_Pratt_smooth(arr)
 end_time = time.time() - start_time
-print "ShellSort (Pratt Smooth) of 10 %% random: %0.5f" % (end_time)
+print "ShellSort (Shell) of %0.2f %% random: \t %0.5f \t %r" % ((100.0) / mix_coeff1, end_time, arr == sorted(arr))
 
-mixelems(5)
+mixelems(arr, 5)
 start_time = time.time()
-ShellSort_Pratt_smooth()
+ShellSort_Pratt_smooth(arr)
 end_time = time.time() - start_time
-print "ShellSort (Pratt Smooth) of 5 random: %0.5f" % (end_time)
+print "ShellSort (Shell) of %d random: \t\t %0.5f \t %r" % (mix_coeff2, end_time, arr == sorted(arr))
